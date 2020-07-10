5335ab086dbe8266fdc83e6f43a4dc0eac9de94b
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-07-10 17:54:50 -0400

    basic index page functionality

---
+++ public/js/index.js
@@ -0,0 +1,51 @@
+var BACKEND_ADDRESS = 'http://backend.test:8080';
+var ITEMS_ADDRESS = BACKEND_ADDRESS + "/items";
+/**
+ * An abstract wrapper around fetch that can be reused throughout the codebase.
+ * @param address The address to query
+ * @param options The query options (e.g. method type, and additional headers)
+ * @param data the payload of the request if there is one.
+ */
+function executeFetch(address, options, data) {
+    var query = {
+        method: options.method,
+        headers: options.headers
+    };
+    if (data) {
+        query.body = JSON.stringify(data);
+    }
+    return fetch(address, query).then(function (res) { return res.json(); });
+}
+var errorElement = document.querySelector("#error-message");
+function postNewItem(item) {
+    var options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    executeFetch(ITEMS_ADDRESS, options, item).then(function (response) {
+        if (!response.id) {
+            errorElement.classList.remove('invisible');
+        }
+        else {
+            errorElement.classList.add('invisible');
+            alert("New Item added to Cart.");
+        }
+    });
+}
+var form = document.querySelector('#input-form');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var title = form['title'].value;
+    var author = form['author'].value;
+    var price = form['price'].value;
+    var count = form['count'].value;
+    var book = {
+        title: title,
+        author: author,
+        price: price,
+        count: count
+    };
+    postNewItem(book);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,77 @@
-interface Item {
+const BACKEND_ADDRESS = 'http://backend.test:8080';
+const ITEMS_ADDRESS = BACKEND_ADDRESS + "/items";
+
+interface Book {
     title: string;
     author: string;
     price: number;
     count: number;
 }
+
+interface FetchOptions {
+    method: string;
+    headers: object;
+}
+
+/**
+ * Represents a plain old JavaScript object. Apparently, this is necessary to satisfy typescript and use
+ * POJOs in the way I want to be able to.
+ */
+interface POJO {
+    [key: string]: any;
+}
+
+/**
+ * An abstract wrapper around fetch that can be reused throughout the codebase.
+ * @param address The address to query
+ * @param options The query options (e.g. method type, and additional headers)
+ * @param data the payload of the request if there is one.
+ */
+function executeFetch(address: string, options: FetchOptions, data?: object): Promise<POJO> {
+    let query: POJO = {
+        method: options.method,
+        headers: options.headers
+    };
+    
+    if (data) {
+        query.body = JSON.stringify(data);
+    }
+    return fetch(address, query).then(res => res.json());
+}
+
+let errorElement = document.querySelector("#error-message");
+
+function postNewItem(item: Book) {
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    executeFetch(ITEMS_ADDRESS, options, item).then(response => {
+        if (!response.id) {
+            errorElement.classList.remove('invisible');
+        } else {
+            errorElement.classList.add('invisible');
+            alert("New Item added to Cart.");
+        }
+    });
+}
+
+let form = document.querySelector('#input-form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let title = form['title'].value;
+    let author = form['author'].value;
+    let price = form['price'].value;
+    let count = form['count'].value;
+
+    let book: Book = {
+        title: title,
+        author: author,
+        price: price,
+        count: count
+    }
+
+    postNewItem(book);
+});

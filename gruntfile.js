4559e9db9f5cb52270006946d7736305bf1428c8
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-07-10 16:15:07 -0400

    adds tsc task to compile .ts files

--- gruntfile.js
+++ gruntfile.js
@@ -3,13 +3,29 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         compass: {
             options: {
-                sassDir: 'assets/sass',
-                cssDir: 'public'
+                sassDir: 'assets/sass/',
+                cssDir: 'public/css/'
             }
         }
     })
 
+    const { exec } = require('child_process');
+
+    grunt.registerTask('tsc', 'runs the typescript compiler', function() {
+        exec('tsc', (error, stdout, stderr) => {
+            if (error) {
+                grunt.log.error('There was an error running `tsc`');
+                grunt.log.error(error.message);
+            }
+
+            if (stderr) {
+                grunt.log.error('tsc failed to compile:');
+                grunt.log.error(stderr);
+            }
+        });
+    });
+
     grunt.loadNpmTasks('grunt-contrib-compass');
 
-    grunt.registerTask('default', 'compass');
+    grunt.registerTask('default', ['compass', 'tsc']);
 };

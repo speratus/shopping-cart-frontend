74709c7be520e924b8f7937a4b0283ff420442df
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-07-11 14:15:03 -0400

    adds docstrings

--- src/cart.ts
+++ src/cart.ts
@@ -1,12 +1,20 @@
 let cart = document.querySelector('#cart-body');
 let totalEl = document.querySelector('#total');
 
+/**
+ * creates a table data element with the specified contents. Reduces repetitive code.
+ * @param contents the text ccontents of the new td element
+ */
 function createTD(contents: any): Element {
     let td = document.createElement('td');
     td.textContent = contents;
     return td;
 }
 
+/**
+ * Adds the specified elements to a table row. Reduces repetitive code.
+ * @param cells the cells to add the table row
+ */
 function createTR(...cells: Element[]): Element {
     let tr = document.createElement('tr');
     for (let td of cells) {
@@ -15,6 +23,10 @@ function createTR(...cells: Element[]): Element {
     return tr;
 }
 
+/**
+ * Sends a request to the backend to delete an item.
+ * @param id the id of the item to delete
+ */
 function deleteItem(id: string) {
     let options = {
         method: 'DELETE',
@@ -22,9 +34,14 @@ function deleteItem(id: string) {
             'Accept': 'application/json'
         }
     }
+    //This line prints an error to the console, but that is expected.
     executeFetch(ITEMS_ADDRESS + "/" + id, options).then(data => getBooks());
 }
 
+/**
+ * Creates a delete button wrapped by a td element. helps keep fillCart function clean and readable.
+ * @param item the data item for which to create a delete button
+ */
 function createDeleteButton(item: POJO): Element {
     let td = document.createElement('td');
     let button = document.createElement('button');
@@ -38,6 +55,10 @@ function createDeleteButton(item: POJO): Element {
     return td;
 }
 
+/**
+ * populates the cart table with the specified data.
+ * @param data the data with which to populate the table
+ */
 function fillCart(data: Array<POJO>) {
     let total = 0;
     cart.innerHTML = "";
@@ -58,6 +79,9 @@ function fillCart(data: Array<POJO>) {
     totalEl.textContent = total.toString();
 }
 
+/**
+ * Queries teh backend for the list of items available.
+ */
 function getBooks() {
     let options = {
         method: 'GET',
